41dea27ab06fb57c8a44a90af6714afef5880803
Adrian Sudbury <sudburya@gmail.com>
2018-02-18 18:52:00 +0000

    Added code for search.

---
+++ PostalAddressSearch/Models/AddressModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Scorchio.PostalAddressSearch.Models
+{
+    public class AddressModel 
+    {
+        /// <summary>
+        /// Gets or sets the building number.
+        /// </summary>
+        public string BuildingNumber { get; set; }
+
+        /// <summary>
+        /// Gets or sets the post code.
+        /// </summary>
+        public string PostCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the full address.
+        /// </summary>
+        public string Address { get; set; }
+    }
+}
